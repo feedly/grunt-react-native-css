677ed97388473aa514ae9b2121e9ed3c06ec4dbc
Alexander Micklewright <alex@feedly.com>
2016-03-24 17:56:15 -0700

    Improve async task management

--- README.md
+++ README.md
@@ -94,6 +94,10 @@ In lieu of a formal styleguide, take care to maintain the existing coding style.
 
 ## Release History
 
+### V 0.1.3 Async update
+
+Improve reliability of async task
+
 ### V 0.1.1 Initial Release
 
 Integrates with [alexmick/react-native-css](https://github.com/alexmick/react-native-css), waiting for PR [#28](https://github.com/sabeurthabti/react-native-css/pull/28) to switch to official npm module
--- tasks/react_native_css.js
+++ tasks/react_native_css.js
@@ -9,39 +9,34 @@
 'use strict';
 
 var RNC = require('react-native-css');
-var temp = require('tmp');
 var path = require('path');
 
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('react_native_css', 'A wrapper for react-native-css', function() {
+    var done = this.async();
+
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       pretty: false,
     });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.linefeed);
+    var files = this.files.slice();
 
-      var tmpfile = temp.fileSync({
-        postfix : '.scss',
-      });
+    function process() {
+      if(files.length <= 0) {
+        done();
+        return;
+      }
 
-      // Write to temp file.
-      grunt.file.write(tmpfile.name, src);
+      var f = files.pop();
+
+      if (f.src.length > 1) {
+        grunt.warn(
+          'Due to limitation in react-native-css, output files are limited to one source file at a time.' +
+          ' Make multiple output files with one source file each.'
+        );
+      }
 
       // Check destination exists
       if (!grunt.file.exists(path.dirname(f.dest))) {
@@ -49,10 +44,13 @@ module.exports = function(grunt) {
       }
 
       var rncss = new RNC();
-      rncss.parse(tmpfile.name, f.dest, options.pretty, function (css) {
+      rncss.parse(f.src[0], f.dest, options.pretty, function () {
         grunt.log.writeln('File "' + f.dest + '" created');
+        process();
       });
-    });
-  });
+    }
 
+    process();
+
+  });
 };
